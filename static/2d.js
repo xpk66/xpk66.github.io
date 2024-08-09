6d6752c3c8936f7387864f67c524accf2c822dce
xpk <101401163+xpk66@users.noreply.github.com>
2024-08-09 12:15:52 +0800

    Delete static/2d.js

--- static/2d.js
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css"> 
-<script src="https://fastly.jsdelivr.net/gh/lrplrplrp/bkyl2d@main/loads.js"></script>
